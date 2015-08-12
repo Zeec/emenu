6b502bdd016bd511b85b783ef29e848f76700372
Zee <zemskov@tillypad.ru>
2015-08-12 12:06:52 +0300

    1

---
+++ _update.bat
@@ -0,0 +1,7 @@
+rem Обновление eMenu
+set server_name=nb-spb-tpad03
+set database_name=DBZee_9_4_4
+set user_name=sa
+set user_password=sasa
+
+sqlcmd -S %server_name% -d %database_name% -U %user_name% -P %user_password% -i em_ExportItemList.sql 
